936bed398c8ddb6e67fcf1272218545f365e8046
Abdul Aziz <abdulaziz5611@gmail.com>
2025-03-19 16:27:00 +0500

    WIP

---
+++ lib/features/quran/domain/entities/ayah_entity.dart
@@ -0,0 +1,7 @@
+class Ayah {
+  final int ayahNumber;
+  final String text;
+  final int surahId;
+
+  Ayah({required this.ayahNumber, required this.text, required this.surahId});
+}
---
+++ lib/features/quran/domain/entities/surah_entity.dart
@@ -0,0 +1,15 @@
+class Surah {
+  final int id;
+  final String nameArabic;
+  final String nameEnglish;
+  final int totalAyahs;
+  final String revelationType;
+
+  Surah({
+    required this.id,
+    required this.nameArabic,
+    required this.nameEnglish,
+    required this.totalAyahs,
+    required this.revelationType,
+  });
+}
